2f2e7f8c1ea019e30ae48cbf2ec010feb25552aa
Simer <singhsimer09@gmail.com>
2022-08-21 19:00:48 -0700

    adding some changes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
 // import "./App.css"
 import Header from "./components/Header"
-import Body from "./components/Body"
+import Landing from "./components/Landing"
 import Social from "./components/Social"
-import { Flex, Heading, VStack, Spacer } from "@chakra-ui/layout"
-import { IconButton } from "@chakra-ui/button"
-import { useColorMode } from "@chakra-ui/color-mode"
 
-import {
-  FaSun,
-  FaMoon,
-  FaInstagram,
-  FaGithub,
-  FaLinkedin,
-} from "react-icons/fa"
+// import {
+//   FaSun,
+//   FaMoon,
+//   FaInstagram,
+//   FaGithub,
+//   FaLinkedin,
+// } from "react-icons/fa"
 
 function App() {
-  const { colorMode, toggleColorMode } = useColorMode()
-
   return (
-    <VStack p={5}>
-      <Flex w="85%">
-        <Heading ml="8" size="md" fontWeight="semibold" color="cyan.400">
-          Simer
-        </Heading>
-        <Spacer />
-
-        <Spacer />
-        <IconButton ml={2} icon={<FaInstagram />} isRound="true" />
-        <IconButton ml={2} icon={<FaGithub />} isRound="true" />
-        <IconButton ml={2} icon={<FaLinkedin />} isRound="true" />
-        <IconButton
-          ml={8}
-          icon={colorMode === "dark" ? <FaSun /> : <FaMoon />}
-          isRound="true"
-          onClick={toggleColorMode}
-        />
-      </Flex>
+    <div className="App">
+      <Landing />
       <Header />
-      <Body />
       <Social />
-    </VStack>
+    </div>
   )
 }
 
---
+++ src/components/Landing.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Body = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-center bg-cover bg-no-repeat customImg">
+      <div className="text-center">
+        <h1 style={{ color: "white" }}>Hey. I'm </h1>
+        <h1 className="text-8xl text-[#808080] mt-[20px] ml-[-110px] reflections">
+          Simer
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Body
