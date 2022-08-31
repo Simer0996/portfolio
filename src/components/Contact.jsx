3e49ac7baed8de4c0898418c548a70136a5882bf
Simer <singhsimer09@gmail.com>
2022-08-30 20:29:05 -0700

    adding some changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import axios from "axios"
 
 const Contact = () => {
   const [name, setName] = useState("")
   const [phone, setPhone] = useState("")
   const [email, setEmail] = useState("")
+  const [FontColor, setFontColor] = useState("")
+
+
+  useEffect(()=>{
+    window.addEventListener('scroll',()=>{
+    if(window.scrollY>720) {
+      setFontColor("#fff")
+    } else {
+      setFontColor("grey")
+    }
+    })
+  },[])
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -14,33 +26,42 @@ const Contact = () => {
     )
   }
   return (
-    <div className="text-white block w-[30%] h-[400px] m-auto">
+    <div className= "block w-[30%] h-[400px] m-auto" >
       <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col">
-        <label className="text-[#454545] text-2xl  ">
+        <label className="text-[grey] text-2xl" style={{color:`${FontColor}`}}>
           Name
           <input
             type="string"
             onChange={(e) => setName(e.target.value)}
-            className="w-[100%] text-[#ff9760]"
+            className="w-[100%]"
+            style={{color:`${FontColor}`,borderBottom: `2px solid ${FontColor}`}}
           />
         </label>
-        <label className="text-[#454545] text-2xl">
+        <label className="text-[grey] text-2xl" style={{color:`${FontColor}`}}>
           Phone
           <input
             type="string"
             onChange={(e) => setPhone(e.target.value)}
-            className="w-[100%] text-[#ff9760]"
+            className="w-[100%]"
+            style={{color:`${FontColor}`,borderBottom: `2px solid ${FontColor}`}}
           />
         </label>
-        <label className="text-[#454545] text-2xl ">
+        <label className="text-[grey] text-2xl" style={{color:`${FontColor}`}}>
           Email
           <input
             type="string"
             onChange={(e) => setEmail(e.target.value)}
-            className="w-[100%] text-[#ff9760]"
+            className="w-[100%]"
+            style={{color:`${FontColor}`, borderBottom: `2px solid ${FontColor}`}}
           />
         </label>
-        <button className="block m-auto buttonStyling text-xl mt-[10px]">
+        <button className="block m-auto buttonStyling text-xl mt-[10px]" style={{
+  backgroundColor: "black",
+  color: `${FontColor}`,
+  border: `1px solid ${FontColor}`,
+  padding: "5px 10px",
+  borderRadius: "2px",
+}}>
           Submit
         </button>
       </form>
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,8 +4,8 @@ const Body = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-center bg-cover  bg-no-repeat customImg">
       <div className="text-center">
-        <h1 className="text-2xl text-[#454545]">Hey. I'm </h1>
-        <h1 className="text-8xl text-[#454545] mt-[20px] ml-[-210px] reflections">
+        <h1 className="text-2xl text-[grey]">Hey. I'm </h1>
+        <h1 className="text-8xl text-[grey] mt-[20px] ml-[-210px] reflections">
           Simer
         </h1>
       </div>
