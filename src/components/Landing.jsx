e6ee058f46d3c160787d9f297f71c57cc6554d2c
Simer <singhsimer09@gmail.com>
2022-09-16 16:07:02 -0700

    adding some changes

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Body = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-center lg:bg-cover bg-no-repeat customImg sm:bg-contain">
+    <div className="flex items-center justify-center h-screen bg-center lg:bg-cover bg-no-repeat customImg bg-contain">
       <div className="text-center">
         <h1 className="text-2xl text-[grey]">Hey. I'm </h1>
         <h1 className="text-4xl text-[grey] mt-[20px] ml-[-210px] reflections">
