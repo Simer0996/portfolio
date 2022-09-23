828a573625c0443f891d96ff391a8b3d295638b4
Simer <singhsimer09@gmail.com>
2022-09-23 15:04:49 -0700

    adding some changes

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,8 +4,8 @@ const Body = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-center bg-contain lg:bg-cover bg-no-repeat customImg ">
       <div className="text-center">
-        <h1 className="text-l text-[grey]">Hey. I'm </h1>
-        <h1 className="text-4xl lg:text-8xl text-[grey] mt-[20px] ml-[-210px] reflections">
+        <h1 className="text-l lg:text-2xl text-[grey]">Hey. I'm </h1>
+        <h1 className="text-4xl lg:text-8xl text-[grey] lg:mt-[20px] lg:ml-[-210px] reflections">
           Simer
         </h1>
       </div>
