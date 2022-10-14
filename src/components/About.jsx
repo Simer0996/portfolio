38aa249bf1c444b7630015dd1fb428d1b186cd61
Simer <singhsimer09@gmail.com>
2022-10-14 10:24:21 -0700

    fixing mobile view

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,20 +18,21 @@ const About = () => {
 
   return (
     
-    <div className="flex items-center gap-[50px] ml-[150px] flex-wrap">
+    <div className="flex items-center gap-[50px] justify-center flex-wrap">
         <img src={myImage} alt="itsMe" className="w-[180px] border-solid border-[5px] rounded-full " style={{
           borderColor: `${color}`,
           boxShadow:`${boxShadow}` 
         }}/>
-        <div style={{color:`${color}`}} className="w-[70%]"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        <div style={{color:`${color}`}} className="w-[70%]"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
           <button style={{  backgroundColor: "black",
           color: `${color}`,
           border: `1px solid ${color}`,
           padding: "4px 8px",
-          borderRadius: "2px"}} className="block buttonStyling text-xl mt-[15px]">Resume</button>
-          </p>
+          borderRadius: "2px"}} className=" block m-auto mt-[20px] mb-[25px] buttonStyling text-xl ">Resume</button>
+         
         </div>
     </div>
+  
     
   )
 }
