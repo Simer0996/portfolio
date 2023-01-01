a562dafbb09a81efb2869ce2834c483fd6b6e645
Simer Singh <simersingh@Simers-MacBook-Air.local>
2023-01-01 15:32:01 -0800

    adding some changeas

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <HeaderNav />
       <Home />
-      <div style={{ marginTop: "-160px", marginBottom: "180px" }}>
+      <div className="mt-[-250px] lg:mt-[-100px] mb-[150px]">
         <Projects />
       </div>
       <div style={{ marginTop: "-200px", marginBottom: "180px" }}>
