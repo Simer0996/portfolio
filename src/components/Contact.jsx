d83e3a312174071767fd60f791be787952e3cb92
Simer Singh <simersingh@Simers-MacBook-Air.local>
2023-01-02 12:43:57 -0800

    adding validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,25 @@
 import React, { useState, useEffect, useRef } from "react"
-
 import emailjs from '@emailjs/browser';
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { ErrorMessage } from '@hookform/error-message';
+import * as yup from "yup";
+
+const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
+const schema = yup.object().shape({
+  from_Email: yup.string().email().required("Field cannot not be empty"),
+  from_phone: yup.string().matches(phoneRegExp, 'Phone number is not valid'),
+  from_message: yup.string(),
+
+});
 
 const Contact = () => {
   const form = useRef();
   const [FontColor, setFontColor] = useState("")
+  const { register, formState: { errors }, handleSubmit } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
@@ -16,51 +31,65 @@ const Contact = () => {
     })
   }, [])
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    emailjs.sendForm('service_15jye7t', 'template_zurs2gs', form.current, 'E68Hjl1hVkx1g0Oc-')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
-
+  const submitForm = () => {
+    if (errors) {
+      console.log(errors)
+    } else {
+      emailjs.sendForm('service_15jye7t', 'template_zurs2gs', form.current, 'E68Hjl1hVkx1g0Oc-')
+        .then((result) => {
+          console.log(result.text);
+        }, (error) => {
+          console.log(error.text);
+        });
+    }
   }
   return (
     <div className="w-[75%] m-auto block" id="contact">
       <div style={{ display: "flex", justifyContent: "space-between" }} >
 
-        <form ref={form} onSubmit={(e) => handleSubmit(e)} className="flex flex-col w-[100%] lg:w-[60%] lg:none">
+        <form ref={form} onSubmit={handleSubmit(submitForm)} className="flex flex-col w-[100%] lg:w-[60%] lg:none">
           <p className="text-2xl lg:text-5xl text-white heading-styling">Get In Touch</p>
-          <label className="text-[grey] text-2xl mt-[30px]" style={{ color: `${FontColor}` }}>
+          <label className="text-[grey] text-3xl mt-[30px]" style={{ color: `${FontColor}` }}>
             Email
-            <input
+            <input {...register('from_Email', { required: true })}
               type="string"
-              // onChange={(e) => setName(e.target.value)}
+              className="w-[100%]"
+              style={{ color: `${FontColor}`, borderBottom: `2px solid ${FontColor}` }}
+            />
+            <ErrorMessage
+              errors={errors}
               name="from_Email"
-              className="w-[100%]"
-              style={{ color: `${FontColor}`, borderBottom: `2px solid ${FontColor}` }}
+              render={({ message }) => <p className="text-white error-styling text-xl">{message}</p>}
             />
+
           </label>
-          <label className="text-[grey] text-2xl" style={{ color: `${FontColor}` }}>
+          <label className="text-[grey] text-3xl" style={{ color: `${FontColor}` }}>
             Phone
-            <input
+            <input {...register('from_phone', { required: true })}
               type="string"
-              // onChange={(e) => setPhone(e.target.value)}
+              className="w-[100%]"
+              style={{ color: `${FontColor}`, borderBottom: `2px solid ${FontColor}` }}
+            />
+            <ErrorMessage
+              errors={errors}
               name="from_phone"
-              className="w-[100%]"
-              style={{ color: `${FontColor}`, borderBottom: `2px solid ${FontColor}` }}
+              render={({ message }) => <p className="text-white error-styling text-xl">{message}</p>}
             />
+
           </label>
-          <label className="text-[grey] text-2xl" style={{ color: `${FontColor}` }}>
+          <label className="text-[grey] text-3xl" style={{ color: `${FontColor}` }}>
             Message
-            <input
+            <input {...register('from_message', { required: true })}
               type="string"
-              // onChange={(e) => setEmail(e.target.value)}
-              name="from_message"
               className="w-[100%]"
               style={{ color: `${FontColor}`, borderBottom: `2px solid ${FontColor}` }}
             />
+            <ErrorMessage
+              errors={errors}
+              name="from_message"
+              render={({ message }) => <p className="text-white error-styling">{message}</p>}
+            />
+
           </label>
           <button className="block m-auto buttonStyling text-xl mt-[10px]" style={{
             backgroundColor: "black",
@@ -72,7 +101,7 @@ const Contact = () => {
             Submit
           </button>
         </form>
-        <img src="/contact/bulb.png" alt="bulb" className="hidden lg:block lg:w-[400px] " />
+        <img src="/contact/bulb.png" alt="bulb" className="hidden lg:block lg:w-[400px] lg:h-[450px] " />
       </div>
     </div>
   )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,49 +4,56 @@ const Projects = () => {
 
   const projects = [
     {
-      title: "Product View",
-      description: "proof of concept",
+      id: "1",
+      title: "Pomodee",
+      description: "Pomodee",
       image: "img/pomodee.jpg",
-      link: "https://ecommerce-olive.vercel.app/",
+      link: "https://pomodee.com",
     },
     {
-      title: "Rest Countries API",
-      description: "proof of concept",
+      id: "2",
+      title: "HandyChef",
+      description: "HandyChef",
       image: "img/handychef.jpg",
-
-      link: "https://my-tailwind-project.vercel.app/",
+      link: "https://www.handychef.ca/",
 
     },
     {
+      id: "3",
       title: "Splink",
       description: "Mobile App",
       image: 'img/pomodee.jpg',
       link: "https://www.splink.ca/",
     },
     {
+      id: "4",
       title: "Dustypaws ",
       description: "Product",
       image: "img/handychef.jpg",
       link: "https://dustypaws.co/",
     },
     {
+      id: "5",
       title: "People and Cars",
       description: "proof of concept",
       image: "img/pomodee.jpg",
       link: "https://dustypaws.co/",
     },
     {
+      id: "6",
       title: "AI web app",
       description: "proof of concept",
       image: "img/handychef.jpg",
       link: "https://dustypaws.co/",
-    },{
+    }, {
+      id: "7",
       title: "Product View",
       description: "proof of concept",
       image: "img/pomodee.jpg",
       link: "https://ecommerce-olive.vercel.app/",
     },
     {
+      id: "8",
       title: "Rest Countries API",
       description: "proof of concept",
       image: "img/handychef.jpg",
@@ -55,24 +62,28 @@ const Projects = () => {
 
     },
     {
+      id: "9",
       title: "Splink",
       description: "Mobile App",
       image: 'img/pomodee.jpg',
       link: "https://www.splink.ca/",
     },
     {
+      id: "10",
       title: "Dustypaws ",
       description: "Product",
       image: "img/handychef.jpg",
       link: "https://dustypaws.co/",
     },
     {
+      id: "11",
       title: "People and Cars",
       description: "proof of concept",
       image: "img/pomodee.jpg",
       link: "https://dustypaws.co/",
     },
     {
+      id: "12",
       title: "AI web app",
       description: "proof of concept",
       image: "img/handychef.jpg",
@@ -81,20 +92,20 @@ const Projects = () => {
   ]
 
   return (
-<>
-<div className="slider m-auto">
-  <div className="slide-track m-5">
-        {projects.map(project => (
-         <div className="slide" key={project.title}>
-            console.log(project.image)
-            <a href={project.link}><img src={project.image} alt={project.title} className="imgBoxShadow" target="blank" />
-          
-            </a>
-          </div>
-    ))}
-</div>
+    <>
+      <div className="slider m-auto">
+        <div className="slide-track m-5">
+          {projects.map(project => (
+            <div className="slide" key={project.id}>
+              console.log(project.image)
+              <a href={project.link}><img src={project.image} alt={project.title} className="imgBoxShadow" target="blank" />
+
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
-</>
+    </>
   )
 };
 
