f2c8ced84ef4d260b103b297d87659356c47c434
Simer Singh <simersingh@Simers-MacBook-Air.local>
2023-01-04 14:03:18 -0800

    fixing some changes

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -8,7 +8,6 @@ const ProjectDetails = ({ projects }) => {
     const { id } = useParams()
     return (
         <div className="block w-[80%] mt-[50px] m-auto">
-
             <Link to="/"><button className="buttonStyling mb-[50px]">Back</button></Link>
             {projects.map((project) => (
                 <div key={project.id}>
@@ -18,8 +17,8 @@ const ProjectDetails = ({ projects }) => {
                             <img src={`/${project.image}`} alt={project.id} className="w-[600px] h-[400px] object-cover block m-auto" />
                             <p className="text-white lg:w-[50%] block m-auto pt-[20px]">{project.description}</p>
                             <div className="flex justify-center align-middle pt-[10px]">
-                                <a href={project.github}><button className="bg-white p-2 buttonStyling">View Source</button></a>
-                                <a href={project.link}><button className="bg-white p-2 ml-[20px] buttonStyling"> Live Site</button></a>
+                                <a href={project.github}><button className="bg-white p-2 buttonStyling" target="_blank" rel="noopener noreferrer">View Source</button></a>
+                                <a href={project.link}><button className="bg-white p-2 ml-[20px] buttonStyling" target="_blank" rel="noopener noreferrer"> Live Site</button></a>
                             </div>
                         </div> : <></>}
                 </div>
